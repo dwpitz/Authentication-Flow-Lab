db456e5e2e10ede9537d88ac42ad394110f85327
David Pitz <dw_pitz@yahoo.com>
2019-10-24 12:57:49 -0500

    dummy index set up

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express()
 require('./db/db')
 
+
+app.get('/', (req, res, next) => {
+	try {
+		console.log('hitting the index route');
+		res.render('index.ejs')
+	}
+	catch (err) {
+		next(err)
+	}
+	})
+
 app.listen(3000, () => {
 	console.log('listening at Port 3000');
-}
-)
+})
