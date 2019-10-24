05efe356a7e070ebea89b65b21d04efe063133dc
David Pitz <dw_pitz@yahoo.com>
2019-10-24 13:30:35 -0500

    registration page

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-	res.send('hitting the user route')
 
+})
+
+//Registration Page
+
+router.get('/new', (req, res, next) => {
+	console.log('hitting registration');
+	res.render('users/new.ejs')
 })
 
 
